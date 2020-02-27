78cccc6349c04d9ac1aeced5be450a620b254dfc
dunkelweizen <53185634+dunkelweizen@users.noreply.github.com>
2020-02-26 22:47:40 -0500

    created tables in ElephantSQL database - don't run this again unless you want to delete everything in the database!

---
+++ create_database.py
@@ -0,0 +1,27 @@
+import os
+import psycopg2
+from dotenv import load_dotenv
+from Data_Science.models import BedHours, Mood, Tiredness, db
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker
+from sqlalchemy import MetaData
+
+load_dotenv()
+
+DATABASE_URL = os.getenv("DATABASE_URL")
+DATABASE_USER = os.getenv("DATABASE_USER")
+DATABASE_PASSWORD = os.getenv("DATABASE_PASSWORD")
+DATABASE_HOST = os.getenv("DATABASE_HOST")
+conn = psycopg2.connect(dbname=DATABASE_USER, user=DATABASE_USER, host=DATABASE_HOST, password=DATABASE_PASSWORD)
+engine = create_engine(DATABASE_URL)
+Session = sessionmaker(bind=engine)
+session = Session()
+meta=MetaData()
+
+meta.drop_all(engine)
+meta.create_all(engine)
+
+
+
+
+
