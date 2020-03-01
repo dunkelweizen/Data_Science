9d9f205187c5120d97451a0d02e13a8e8426cbbe
dunkelweizen <53185634+dunkelweizen@users.noreply.github.com>
2020-02-29 22:32:06 -0500

    created averages function for smaller dataset

--- analysis.py
+++ analysis.py
@@ -86,11 +86,57 @@ def hours_analysis(username):
     model.fit(X, y)
 
     optimal_hours = model.predict([[4, 4, 4, 1, 1, 1]]) / np.timedelta64(1, 'h')
-    return optimal_hours
+    averages = np.array([df['hours_in_bed'].mean().seconds/3600, (sum(morning_mood_list)/len(morning_mood_list)), (sum(midday_mood_list)/len(midday_mood_list)),
+                (sum(evening_mood_list)/len(evening_mood_list)), (sum(morning_tired_list)/len(morning_tired_list)),
+                (sum(midday_tired_list)/len(midday_tired_list)), (sum(evening_tired_list)/len(evening_tired_list))])
+    return optimal_hours, averages
 
 
 def sleep_averages(username):
     user_id = connection.execute(select([User.id]).where(User.username == username))
     for result in user_id:
         user_id = result[0]
-    return user_id
+    print('getting bedtimes and waketimes')
+    bedtimes = connection.execute(select([BedHours.bedtime]).where(BedHours.user_id == user_id))
+    bedtimes_list = []
+    for result in bedtimes:
+        bedtimes_list.append(result[0])
+    bedtimes_ids = connection.execute(select([BedHours.id]).where(BedHours.user_id == user_id))
+    bedtimes_ids_list = []
+    for result in bedtimes_ids:
+        bedtimes_ids_list.append(result[0])
+    waketimes = connection.execute(select([BedHours.waketime]).where(BedHours.user_id == user_id))
+    waketimes_list = []
+    for result in waketimes:
+        waketimes_list.append(result[0])
+    morning_mood_list = []
+    midday_mood_list = []
+    evening_mood_list = []
+    morning_tired_list = []
+    midday_tired_list = []
+    evening_tired_list = []
+    print('getting moods and tiredness')
+    for id in bedtimes_ids_list:
+        mood = connection.execute(select([Mood.wake_mood, Mood.midday_mood, Mood.night_mood]).where(Mood.night_id == id))
+        for result in mood:
+            morning_mood_list.append(result[0])
+            midday_mood_list.append(result[1])
+            evening_mood_list.append(result[2])
+        tired = connection.execute(select(
+            [Tiredness.wake_tired, Tiredness.midday_tired, Tiredness.night_tired]).where(Tiredness.night_id == id))
+        for result in tired:
+            morning_tired_list.append(result[0])
+            midday_tired_list.append(result[1])
+            evening_tired_list.append(result[2])
+    hours_in_bed = []
+    for i in range(len(bedtimes_ids_list)):
+        hours = waketimes_list[i] - bedtimes_list[i]
+        hours_in_bed.append(hours.seconds / 3600)
+    avg_hours_in_bed = (sum(hours_in_bed)/len(hours_in_bed))
+    averages = np.array([avg_hours_in_bed, (sum(morning_mood_list) / len(morning_mood_list)),
+                         (sum(midday_mood_list) / len(midday_mood_list)),
+                         (sum(evening_mood_list) / len(evening_mood_list)),
+                         (sum(morning_tired_list) / len(morning_tired_list)),
+                         (sum(midday_tired_list) / len(midday_tired_list)),
+                         (sum(evening_tired_list) / len(evening_tired_list))])
+    return averages
